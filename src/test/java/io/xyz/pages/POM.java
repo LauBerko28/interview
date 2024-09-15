21385de43cf2fb9c99cddde37bf22e96dfb0ff02
LauBerko28 <laura.vargas129@gmail.com>
2024-09-15 17:58:49 -0400

    third commit

--- src/test/java/io/xyz/pages/POM.java
+++ src/test/java/io/xyz/pages/POM.java
@@ -1,4 +1,15 @@
 package io.xyz.pages;
 
+import io.xyz.layers.common.pages.ui.UiBasePage;
+
 public class POM {
+
+    private UiBasePage uiBasePage;
+
+    public UiBasePage createInstanceOfBasePage() {
+        if(uiBasePage == null) {
+            uiBasePage = new UiBasePage();
+        }
+        return uiBasePage;
+    }
 }
--- src/test/java/io/xyz/utils/common_utils/Environment.java
+++ src/test/java/io/xyz/utils/common_utils/Environment.java
@@ -1,4 +1,36 @@
 package io.xyz.utils.common_utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Dynamically loads test data to variables, according to the env value.
+ * First checks if "env" value was passed from maven command line.
+ * If yes, it will use that value.
+ * If not, it will use value in local configuration.properties file
+ */
+
 public class Environment {
-}
+    public static final String URL;
+
+    static { //runs once in beginning when we use the class- static block
+        //class to read from .properties files
+        Properties properties = null;
+        String environment = System.getProperty("env" ) != null ? System.getProperty("env" ) : ConfigurationReader.getProperties("env" );
+        //String environment = ConfigurationReader.get("environment");
+
+        try {
+
+            String path = System.getProperty("user.dir" ) + "/src/test/resources/env/" + environment + ".properties";
+
+            FileInputStream input = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(input);
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        URL = properties.getProperty("url" );
+}}
